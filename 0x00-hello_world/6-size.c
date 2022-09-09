e9138d65beb4739d8aa6fb8537d13a7822f68fb7
Nanagyamera <nanagyamera3@gmail.com>
2022-09-08 17:38:54 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %1d byte(s)\n", sizeof(c));
 	printf("Size of an int: %1d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %1d byte(s)\n", sizeof(l));
-	printf("Size of a long long int: %1d byte(s)\n", sizeof(t));
-	printf("Size of a float: %1d byte(s)\n", sizeof(a));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %1d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %1d byte(s)\n", sizeof(f));
 	return (0);
 }
