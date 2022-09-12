1904d72b17f7f90469cd1d32f6bc311fa40b887d
Nanagyamera <nanagyamera3@gmail.com>
2022-09-12 07:51:00 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the strings in the put function 
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", size of(f));
+	return (0);
+}
