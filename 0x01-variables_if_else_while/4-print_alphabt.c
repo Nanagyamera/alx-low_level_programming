754be9f17cd7cba614886fc4b814e3ebeccb378f
Nanagyamera <nanagyamera3@gmail.com>
2022-09-12 08:51:44 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - print if the number is postive, zero, or negative  
+ * main - print if the number is postive, zero, or negative  
  *
- *      * Return: 0
- *
+ * Return: 0
  */
 
 int main(void)
