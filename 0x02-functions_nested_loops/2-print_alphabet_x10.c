f214ac02a61f9bafd4bb43462ae5114fb23018a1
Nanagyamera <nanagyamera3@gmail.com>
2022-09-13 14:36:14 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print the alphabet 10 times
+ *
+ * Return: Success
+ */
+void print_alphabet-x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for {c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
