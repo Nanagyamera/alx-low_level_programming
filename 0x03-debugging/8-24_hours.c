aeaa6e81217b326c86c857eab87fdce0481f7bcb
Nanagyamera <nanagyamera3@gmail.com>
2022-09-14 14:50:56 -0700

    task 8

--- 0x03-debugging/8-24_hours.c
+++ 0x03-debugging/8-24_hours.c
@@ -2,6 +2,7 @@
 
 /**
  * jack_bauer - function that prints every minute of the day of Jack Bauer, starti
+ * n from 00:00 to 23:59, min loop counts mins, whiles hours loop counts hours
  * Return: 0
  */
 void jack_bauer(void);
