c35a0c5bbd3d5e6cab9465de3d6e09d930ef710e
Nanagyamera <nanagyamera3@gmail.com>
2022-09-13 17:03:24 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@ include "main.h"
 
 /**
  * _islower - function to check for lowercase charater
- * 
+ * @c: a character argument
  * Return: returns 1 and 0 depending on the condition
  */
 int _islower(int c)
