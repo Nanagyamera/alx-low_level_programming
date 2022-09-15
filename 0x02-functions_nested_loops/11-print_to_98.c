75af21f1356bc9621ceb10512212182877bcfe4d
Nanagyamera <nanagyamera3@gmail.com>
2022-09-14 18:39:12 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,14 +8,14 @@ void print_to_98(int n)
 {
 	if (n >= 0)
 	{
-		while(n >= 98)
+		while (n > 98)
 			printf("%d, ", n --);
 		printf("%d\n", n);
 	}
 	else
 	{
-		where(n < 98)
+		while (n < 98)
 			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
-i}
+}
