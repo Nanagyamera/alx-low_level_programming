db3d2967f14ae259e300584952ac5e2ed6929a9b
Nanagyamera <nanagyamera3@gmail.com>
2022-09-14 17:34:17 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @num1: the first integer to be added.
+ * @num2: the second integer to be created.
+ *
+ * Return: 0
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
