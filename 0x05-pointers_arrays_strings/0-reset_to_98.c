fd90ad54d0342ce857b471ab1e1aa831cac92f5e
Nanagyamera <nanagyamera3@gmail.com>
2022-09-19 09:39:09 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer as a parameter and updates its values
+ * @n: parameter
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
