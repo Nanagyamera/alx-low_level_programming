44d7b2b0bff6c027c6afa9508e914abc45602803
Nanagyamera <nanagyamera3@gmail.com>
2022-09-19 14:20:03 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,11 @@ void print_array(int *a, int n)
 {
 	int inc;
 
-	for (inc = 0; inc < n; inc ++)
+	for (inc = 0; inc < n; inc++)
 	{
 		if (inc != n - 1)
 			printf("%d, ", a[inc]);
-		else 
+		else
 			printf("%d", a[inc]);
 	}
 	putchar(10);
