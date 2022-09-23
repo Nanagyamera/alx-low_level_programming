1723d63bc235b7a8c3234436173adb7d26f826fc
Nanagyamera <nanagyamera3@gmail.com>
2022-09-23 06:53:14 -0700

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 
 int _putchar(char c);
+void print_number(int n);
+void print_buffer(char *b, int size);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,4 +13,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 
-#endif 
+#endif
