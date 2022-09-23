e59f358bf43c5dcaaf25dcc9e22382bab09da896
Nanagyamera <nanagyamera3@gmail.com>
2022-09-23 07:34:52 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,10 @@ void reverse_array(int *a, int n)
 {
 	int x, y;
 
-	for (x =0; x < (n / 2); x++)
+	for (x = 0; x < (n / 2); x++)
 	{
 		y = a[x];
-		a[x] = a[n -x - 1];
+		a[x] = a[n - x - 1];
 		a[n - x - 1] = y;
 	}
 }
