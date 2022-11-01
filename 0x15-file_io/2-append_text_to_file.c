5c35655fdaeade832d7b382a354fb63053260e17
Nanagyamera <nanagyamera3@gmail.com>
2022-10-31 18:05:38 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * append_text_to_file - appends text at the end of a file
@@ -11,8 +10,6 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nletters;
-	int rwr;
 
 	if (!filename)
 		return (-1);
@@ -24,16 +21,27 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters}; nletters++)
-			;
-
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
+		if (write(fd, text_content, _strlen(text_content)) == -1)
 			return (-1);
 	}
 
 	close(fd);
 
 	return (1);
+}
+
+/**
+ * _strlen -the length of a string to return
+ * @s: the string to count
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
